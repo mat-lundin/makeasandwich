c68d56976397ca2ea9ddf3c9e38a55955dc4b742
Mat Lundin <matlundin.dev@gmail.com>
2023-03-15 23:42:51 -0500

    add icons to ingredients list

--- src/components/ingredients.js
+++ src/components/ingredients.js
@@ -2,36 +2,48 @@ import ListGroup from 'react-bootstrap/ListGroup';
 import Button from 'react-bootstrap/Button';
 import React, { useState } from 'react';
 
+const breadImg = process.env.PUBLIC_URL + "/ingredients/bread.png";
+const condimentImg = process.env.PUBLIC_URL + "/ingredients/condiment.png";
+const vegImg = process.env.PUBLIC_URL + "/ingredients/lettuce.png";
+const meatImg = process.env.PUBLIC_URL + "/ingredients/meat.png";
+const cheeseImg = process.env.PUBLIC_URL + "/ingredients/cheese.png";
+
 // all ingredient options
 const ingredientList = [{
     id: 0,
     name: 'white bread',
-    type: 'bread'
+    type: 'bread',
+    icon: breadImg
 },
 {
     id: 1,
     name: 'lettuce',
-    type: 'veg'
+    type: 'veg',
+    icon: vegImg
 },
 {
     id: 2,
     name: 'tomato',
-    type: 'veg'
+    type: 'veg',
+    icon: vegImg
 },
 {
     id: 3,
     name: 'bacon',
-    type: 'meat'
+    type: 'meat',
+    icon: meatImg
 },
 {
     id: 4,
     name: 'mustard',
-    type: 'condiment'
+    type: 'condiment',
+    icon: condimentImg
 },
 {
     id: 5,
     name: 'cheddar',
-    type: 'cheese'
+    type: 'cheese',
+    icon: cheeseImg
 }];
 
 const Ingredients = (props)=>{
@@ -43,7 +55,7 @@ console.log('ingredients rendered')
         <ListGroup>
         {ingredientList.map((ing, index)=>{
             return(
-                <ListGroup.Item key={index}>{ing.name}<Button onClick={()=>props.add(ing.name)}>Add</Button></ListGroup.Item>
+                <ListGroup.Item key={index}>{ing.name}<img className='ingIcon' src={ing.icon}></img><Button onClick={()=>props.add(ing.name)}>Add</Button></ListGroup.Item>
             )
         })}
         </ListGroup>
