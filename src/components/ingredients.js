df96394d7a6d27e854db128bd2c5eb589694ee1a
Mat Lundin <matlundin.dev@gmail.com>
2023-03-21 22:04:58 -0500

    add ingredient search bar for ing name or type

--- src/components/ingredients.js
+++ src/components/ingredients.js
@@ -8,15 +8,23 @@ console.log('ingredients rendered')
 
 // state of ingredient search term
 const [ingSearch, setIngSearch] = useState('')
+
+// update search state
+function updateIngSearch(val){
+    setIngSearch(val);
+    console.log(`ingSearch = ${ingSearch}`)
+}
+
     return(
         <div className='ingredients'>
         <h2>Ingredients</h2>
-        <Form.Control type="text" id="name" placeholder='Search' onFocus={(e)=>e.target.select()}></Form.Control>
+        <Form.Control type="text" id="name" placeholder='Search' onFocus={(e)=>e.target.select()} onChange={(e)=> updateIngSearch(e.target.value)}></Form.Control>
         <ListGroup>
         {props.ingredients.map((ing, index)=>{
-            return(
-                <ListGroup.Item key={index}>{ing.name}<img className='ingIcon' src={ing.icon}></img><Button onClick={()=>props.add(ing)}>Add</Button></ListGroup.Item>
-            )
+            if (ing.name.toLowerCase().includes(ingSearch) || ing.type.toLowerCase().includes(ingSearch))
+            {return(
+                <ListGroup.Item key={index}>{ing.name} <img className='ingIcon' src={ing.icon}></img><Button onClick={()=>props.add(ing)}>Add</Button></ListGroup.Item>
+            )}
         })}
         </ListGroup>
         </div>
