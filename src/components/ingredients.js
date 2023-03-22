c77c85ec0b9a02573c706a5b33758023c8153c98
Mat Lundin <matlundin.dev@gmail.com>
2023-03-21 21:27:47 -0500

    init state, ingredient search field

--- src/components/ingredients.js
+++ src/components/ingredients.js
@@ -1,13 +1,17 @@
 import ListGroup from 'react-bootstrap/ListGroup';
 import Button from 'react-bootstrap/Button';
 import React, { useState } from 'react';
+import Form from 'react-bootstrap/Form';
 
 const Ingredients = (props)=>{
 console.log('ingredients rendered')
 
+// state of ingredient search term
+const [ingSearch, setIngSearch] = useState('')
     return(
         <div className='ingredients'>
         <h2>Ingredients</h2>
+        <Form.Control type="text" id="name" placeholder='Search' onFocus={(e)=>e.target.select()}></Form.Control>
         <ListGroup>
         {props.ingredients.map((ing, index)=>{
             return(
