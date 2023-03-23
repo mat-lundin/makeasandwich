541be3d91709c9aa832a276707c6bb0bde81ad6b
Mat Lundin <matlundin.dev@gmail.com>
2023-03-22 22:30:19 -0500

    display current ingredients as table

--- src/components/CurrentSandwich.js
+++ src/components/CurrentSandwich.js
@@ -2,6 +2,7 @@ import ListGroup from 'react-bootstrap/ListGroup';
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
 import Modal from 'react-bootstrap/Modal';
+import Table from 'react-bootstrap/Table';
 import { useState } from 'react';
 
 const CurrentSandwich = (props)=> {
@@ -28,13 +29,20 @@ const CurrentSandwich = (props)=> {
           </Button>
         </Modal.Footer>
       </Modal>
-        <ListGroup>
+        {/* <ListGroup> */}
+        <Table id="ingTable" borderless={true} hover size="sm">
             {props.sandwich.ingredients.map((ing,index)=>{
                 return(
-                <ListGroup.Item key={index}>{props.displayIngName(ing)}<img className='ingIcon' src={props.displayIngIcon(ing)}></img><Button onClick={()=>props.remove(index)}>Remove</Button></ListGroup.Item>
+                  <tr key={index}>
+                    <td>{props.displayIngName(ing)}</td>
+                    <td><img className='ingIcon' src={props.displayIngIcon(ing)}></img></td>
+                    <td><Button onClick={()=>props.remove(index)}>Remove</Button></td>
+                  </tr>
+                // <ListGroup.Item key={index}>{props.displayIngName(ing)}<img className='ingIcon' src={props.displayIngIcon(ing)}></img><Button onClick={()=>props.remove(index)}>Remove</Button></ListGroup.Item>
                 )
             })}
-        </ListGroup>
+            </Table>
+        {/* </ListGroup> */}
         <Button variant="success" size='lg' onClick={props.sandwich.ingredients.length<1? handleShow : props.save}>Save</Button>
         <Button variant="danger" size='sm' onClick={()=>props.clearCurrent()}>Clear</Button>{' '}
         </div>
