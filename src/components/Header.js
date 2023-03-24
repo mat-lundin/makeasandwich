2f195e63fdb45ad2057933b264144ea41e3f9778
Mat Lundin <matlundin.dev@gmail.com>
2023-03-24 00:56:24 -0500

    make header look better

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,15 @@
 import Container from 'react-bootstrap/Container';
 import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav';
 
 function Header(){
     return (
         <Navbar expand="lg" variant="light" bg="light">
       <Container>
-        <Navbar.Brand href="#">Navbar</Navbar.Brand>
+        <Navbar.Brand><img src={process.env.PUBLIC_URL+'/images/sandwich.png'}></img></Navbar.Brand>
+        <Nav>
+        <h1>Make A Sandwich</h1>
+        </Nav>
       </Container>
     </Navbar>
     )
